09389130eb0478ab97aef446a5dfcef58e207df9
fpilz82 <florian.pilz@posteo.de>
2020-03-09 16:08:36 +0100

    Remove unused code

--- example-jvm/src/main/java/de/p7s1/qa/sevenfacette/db/exception/SqlErrorExecutingStatementException.kt
+++
@@ -1,8 +0,0 @@
-package de.p7s1.qa.sevenfacette.db.exception
-
-class SqlErrorExecutingStatementException : RuntimeException {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
