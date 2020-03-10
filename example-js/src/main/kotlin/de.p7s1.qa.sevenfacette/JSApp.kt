f993f64163a718452b9a4a3c22ba6a10ea0ffe35
fpilz82 <florian.pilz@posteo.de>
2020-03-10 17:01:19 +0100

    Adopt example js to changey

--- example-js/src/main/kotlin/de.p7s1.qa.sevenfacette/JSApp.kt
+++ example-js/src/main/kotlin/de.p7s1.qa.sevenfacette/JSApp.kt
@@ -4,13 +4,9 @@ import de.p7s1.qa.sevenfacette.veritas.all
 import de.p7s1.qa.sevenfacette.veritas.verifyThat
 import de.p7s1.qa.sevenfacette.veritas.verification.hasLength
 import de.p7s1.qa.sevenfacette.veritas.verification.startsWith
-import mu.KotlinLogging
 
 
 fun main() {
-    val logger = KotlinLogging.logger {  }
-
-    logger.info { "Hello World!" }
 
     val statementForExecution = de.p7s1.qa.sevenfacette.db.DbStatements()
     statementForExecution.add("statement")
