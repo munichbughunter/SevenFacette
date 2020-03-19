769f958250b3a9121bf8ad1d057f4615cbccc79f
Patrick Döring <patrick.doering@prosiebensat1.com>
2020-03-19 10:12:24 +0100

    Make test beautiful

--- example-jvm/src/test/java/de/p7s1/qa/sevenfacette/kafka/JKafkaConsumerTest.java
+++ example-jvm/src/test/java/de/p7s1/qa/sevenfacette/kafka/JKafkaConsumerTest.java
@@ -7,20 +7,42 @@ class JKafkaConsumerTest {
 
   @Test
   void consumeMessages() {
-    String topic = "ucp.sg-test-INGEST";
-    KConsumer kafkaConsumer = new KConsumer(topic, 1000, "*", 5);
+    String ingestTopic = "Topic 1";
+    String commitStreamTopic = "Topic 2";
+    String forwardReplicationTopic = "Topic 3";
+    String persistenceTopic = "Topic 4";
 
-    kafkaConsumer.consume();
+    KConsumer kafkaIngestConsumer = new KConsumer(ingestTopic, 1000, "*", 5);
+    KConsumer kafkaCommitStreamConsumer = new KConsumer(commitStreamTopic, 1000, "*", 5);
+    KConsumer kafkaReplicationConsumer = new KConsumer(forwardReplicationTopic, 1000, "*", 5);
+    KConsumer kafkaPersistConsumer = new KConsumer(persistenceTopic, 1000, "*", 5);
+
+    kafkaCommitStreamConsumer.consume();
+    kafkaReplicationConsumer.consume();
+    kafkaPersistConsumer.consume();
+    kafkaIngestConsumer.consume();
 
     System.out.println("Beginne mit einer Action wie DB Zeug anlegen... ");
     System.out.println("Hier bin ich mit dem anlegen fertig... ");
 
-    kafkaConsumer.waitForMessage(5000);
+    System.out.println("AUSGABE COMMITSTREAM CONSUMER");
+    assertTrue(kafkaCommitStreamConsumer.waitForMessage(5000));
+    System.out.println(kafkaCommitStreamConsumer.getMessageCount());
+    //assertEquals(500, kafkaCommitStreamConsumer.getMessageCount());
 
+    System.out.println("AUSGABE REPLICATION CONSUMER");
+    assertTrue(kafkaReplicationConsumer.waitForMessage(5000));
+    System.out.println(kafkaReplicationConsumer.getMessageCount());
+    //assertEquals(500, kafkaReplicationConsumer.getMessageCount());
 
-    assertTrue(kafkaConsumer.hasMessage());
-    assertEquals(500, kafkaConsumer.getMessageCount());
+    System.out.println("AUSGABE PERSIST CONSUMER");
+    assertTrue(kafkaPersistConsumer.waitForMessage(5000));
+    System.out.println(kafkaPersistConsumer.getMessageCount());
+    //assertEquals(500, kafkaPersistConsumer.getMessageCount());
 
-    System.out.println(kafkaConsumer.getMessageList().size());
+    System.out.println("AUSGABE INGEST CONSUMER");
+    assertTrue(kafkaIngestConsumer.waitForMessage(5000));
+    System.out.println(kafkaIngestConsumer.getMessageCount());
+    //assertEquals(500, kafkaIngestConsumer.getMessageCount());
   }
 }
