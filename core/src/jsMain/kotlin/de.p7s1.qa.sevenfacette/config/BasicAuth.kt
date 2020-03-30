eda53d96b5a9841df3f99fcfe88d314165d21fd8
fpilz82 <florian.pilz@posteo.de>
2020-03-30 16:39:21 +0200

    Remove unused code

--- core/src/jsMain/kotlin/de.p7s1.qa.sevenfacette/config/BasicAuth.kt
+++
@@ -1,4 +0,0 @@
-package de.p7s1.qa.sevenfacette.config
-
-actual class BasicAuth actual constructor(serviceName: String, username: String?, password: String?) : RestServiceAuth
-actual class LDAPAuth actual constructor(username: String, password: String) : RestServiceAuth
