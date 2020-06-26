5d1fd4babb6b3adc461fdae8dd0a49f29b4cec4a
Patrick Döring <patrick.doering@prosiebensat1.com>
2020-06-26 07:47:31 +0200

    Verify screenshotting

--- example-jvm/src/test/java/de/p7s1/qa/sevenfacette/selenium/SeleniumDriverTest.java
+++ example-jvm/src/test/java/de/p7s1/qa/sevenfacette/selenium/SeleniumDriverTest.java
@@ -4,8 +4,13 @@ package de.p7s1.qa.sevenfacette.selenium;
 import de.p7s1.qa.sevenfacette.driver.FDFactory;
 import de.p7s1.qa.sevenfacette.driver.FDFactory.Driver;
 import de.p7s1.qa.sevenfacette.driver.FDriver;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
 
 /**
  * TODO: Add Description
@@ -13,12 +18,30 @@ import org.openqa.selenium.WebDriver;
  * @author Patrick Döring
  */
 public class SeleniumDriverTest {
-  @Test
-  void startBrowser() {
-    System.setProperty("webdriver.chrome.driver","/Users/doe0003p/TestLab/p7s1-qa/os/SevenFacette/chromedriver");
 
-    FDriver facettedriver = new FDriver(FDFactory.driver(Driver.CHROME));
-    facettedriver.get("https://www.google.de");
-    facettedriver.quit();
+  private static FDriver facetteDriver;
+  public static final String SCREENSHOT_OUTPUT_DIRECTORY = "./build/screenshots/";
+
+  @BeforeAll
+  static void setup() {
+    System.setProperty("webdriver.chrome.driver","/Users/doe0003p/TestLab/p7s1-qa/os/SevenFacette/chromedriver");
+    facetteDriver = new FDriver(FDFactory.driver(Driver.CHROME));
+  }
+
+  @AfterAll
+  static void tearDown() {
+    facetteDriver.quit();
+  }
+
+  @Test
+  void startFacetteBrowser() {
+    facetteDriver.get("https://www.google.de");
+  }
+
+  @Test
+  void makeScreenshot() throws IOException {
+    facetteDriver.get("https://www.google.de");
+    File screeny = facetteDriver.getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(screeny, new File(SCREENSHOT_OUTPUT_DIRECTORY + "FDriverTest.png"));
   }
 }
