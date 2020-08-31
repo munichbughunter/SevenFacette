186f6f53be10412e6a1ea2f85f20fa0a24369bda
Patrick Döring <patrick.doering@prosiebensat1.com>
2020-08-31 11:46:15 +0200

    Delete unused files and change readme

--- core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/Base64.kt
+++
@@ -1,5 +0,0 @@
-package de.p7s1.qa.sevenfacette
-
-interface Base64Encoder {
-    fun encode(src: ByteArray): ByteArray
-}
--- core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/Base64Factory.kt
+++
@@ -1,5 +0,0 @@
-package de.p7s1.qa.sevenfacette
-
-expect object Base64Factory {
-    fun createEncoder(): Base64Encoder
-}
