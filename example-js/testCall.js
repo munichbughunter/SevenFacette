7ad6564d66ec306bbe58fc71707ebbca5ea1623c
Patrick Döring <patrick.doering@prosiebensat1.com>
2020-09-04 06:52:05 +0200

    Add factory and producer implementation

--- example-js/testCall.js
+++ example-js/testCall.js
@@ -1,7 +1,22 @@
 const sevenfacette = require('../build/js/packages/SevenFacette-core').de.p7s1.qa.sevenfacette.kafka;
 
 async function testCall() {
-  const producer = sevenfacette.createProducer();
+
+  var kConfig = new sevenfacette.config.KConfig();
+  kConfig.autoOffset = 'earliest';
+  kConfig.bootstrapServer = 'localhost:9092';
+  console.log(kConfig.autoOffset);
+
+  var producerConfig = new sevenfacette.config.KTableTopicConfig(kConfig);
+  producerConfig.kafkaTopic = 'testTopic';
+  console.log(producerConfig.kafkaTopic);
+  console.log(producerConfig.kafkaConfig.bootstrapServer);
+
+  const kProducer = producerConfig.createKProducer();
+  console.log(kProducer);
+
+  kProducer.send("message to send");
+
   console.log("producer call is working");
 }
 testCall();
