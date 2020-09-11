9d7f11bb3fdd01260ea24d758aca52325bffe53a
fpilz82 <florian.pilz@posteo.de>
2020-09-11 15:07:11 +0200

    Add documentation

--- core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/config/types/FacetteConfigDataClass.kt
+++ core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/config/types/FacetteConfigDataClass.kt
@@ -3,11 +3,14 @@ package de.p7s1.qa.sevenfacette.config.types
 import kotlinx.serialization.Serializable
 
 /**
- * Overall type for configurations. The config yaml files will be mapped to this type.
  * Add more properties if config yaml/ json will be extended and map them to FacetteConfig-object.
  *
- * @property httpClients List of httpClientConfiguration
+ * @property http List of httpConfigs
+ * @property kafka List of kafkaConfigs
+ * @property database List of databaseConfigs
+ * @property application List of applicationConfigs. Is used to combine with string boot projects
  * @property custom map to give the users the possibility to add custom configurations
+ * @property web configurations for web project
  *
  * @author Florian Pilz
  */
--- core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/config/types/HttpConfig.kt
+++ core/src/commonMain/kotlin/de/p7s1/qa/sevenfacette/config/types/HttpConfig.kt
@@ -2,9 +2,12 @@ package de.p7s1.qa.sevenfacette.config.types
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Contains map of httpClientConfigs. The coonfigurations can be accessed via name.
+ */
 @Serializable
 data class HttpConfig(
-        val clients: Map<String, HttpClientConfig>
+    val clients: Map<String, HttpClientConfig>
 ) {
-    fun getClient(clientName: String): HttpClientConfig? = clients.get(clientName)
+    fun getClient(clientName: String): HttpClientConfig? = clients[clientName]
 }
