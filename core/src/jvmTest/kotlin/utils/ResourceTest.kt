6b46157dc259f12fa5d77c031f41b7a5ed559368
StellaB <stella@bastug.org>
2020-10-29 16:24:48 +0100

    rename testing file ResourceTest.kt tests

--- core/src/jvmTest/kotlin/utils/ResourceTest.kt
+++ core/src/jvmTest/kotlin/utils/ResourceTest.kt
@@ -3,20 +3,41 @@ package utils
 
 import de.p7s1.qa.sevenfacette.utils.Resource
 import org.junit.Test
+import java.io.File
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 /**
  * Testingclass for [Resource].
  *
  * Testcases:
- *
+ * - Can i get a resource (return isNotNull)
+ * - Can i getting the right full path
+ * 
  * @author Stella Bastug
  */
 class ResourceTest {
+    // relative path specifically for the test case
+    private val RESOURCES_TEST_FOLDER_PATH = "\\src\\jvmTest\\resources\\testfiles\\test.txt"
 
     @Test
-    fun getResourceTest(){}
+    fun getResourceTest(){
+        val resource = Resource.get(RESOURCES_TEST_FOLDER_PATH)
+
+        assertNotNull(resource)
+    }
 
     @Test
-    fun getFullPathTest(){}
+    fun getFullPathTest(){
+        //arrange
+        val fullPath = Resource.getFullPath(RESOURCES_TEST_FOLDER_PATH)
+        val expectedPath = Paths.get(System.getProperty("user.dir")).toString().plus(RESOURCES_TEST_FOLDER_PATH)
+        //act
+        val dirPathLength = Paths.get(System.getProperty("user.dir")).toString().length
+        //assert
+        assertEquals(expectedPath.substring(dirPathLength),fullPath.substring(dirPathLength))
+    }
 
 }
