8dbda78e03fcc9d9163c07b55d70215e548f2c57
munichbughunter <munichbughunter@gmail.com>
2020-11-02 13:33:12 +0100

    Add dbstatements

---
+++ core/src/jvmMain/kotlin/de/p7s1/qa/sevenfacette/db/DbStatements.kt
@@ -0,0 +1,28 @@
+package de.p7s1.qa.sevenfacette.db
+
+import java.util.Collections
+
+actual class DbStatements {
+
+    actual val statements: MutableList<String> = mutableListOf()
+
+    actual fun add(statement: String): Boolean {
+        return statements.add(statement)
+    }
+
+    actual val list: List<String>
+        get() = Collections.unmodifiableList(statements)
+
+
+    actual operator fun contains(statement: String): Boolean {
+        return statement in statements
+    }
+
+    actual operator fun get(index: Int): String {
+        return statements[index]
+    }
+
+    actual fun size(): Int {
+        return statements.size
+    }
+}
--- example-js/testCall.js
+++ example-js/testCall.js
@@ -1,11 +1,12 @@
 const sfKafka = require('../build/js/packages/SevenFacette-core').de.p7s1.qa.sevenfacette.kafka;
 const sfConfig = require('../build/js/packages/SevenFacette-core').de.p7s1.qa.sevenfacette.config;
-
 // ES6
 //import {createConsumer} from "../build/js/packages/SevenFacette-core').de.p7s1.qa.sevenfacette.kafka";
 //import consi from "../build/js/packages/SevenFacette-core').de.p7s1.qa.sevenfacette.kafka";
 
 async function testConsumer() {
+
+
 
   var kConfig = new sfConfig.types.KafkaTopicConfig();
   kConfig.autoOffset = true;
@@ -20,47 +21,47 @@ async function testConsumer() {
   kConfig.groupID = '';
   kConfig.kafkaProtocol = '';
 
-  // var producer = new sfKafka.KProducer(kConfig).createKProducer();
-  // await producer.sendKeyMessage("Testmessage", "Here I am rocking like a hurricane");
-  // setTimeout(() => {
-  //   producer.disconnect();
-  // }, 5000);
-
-  //const consumer = createConsumer(kConfig);
-  //consi.createKConsumer(kConfig);
-  const kafka = new sfKafka.KConsumer(kConfig).createKConsumer();
-  const sfConsumer = kafka.getConsumer();
-
-  await sfConsumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      //const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-      //console.log(`- ${prefix} ${message.key}#${message.value}`)
-      kafka.addKRecord(message.key.toString(), message.value.toString(), message.offset, partition)
-    },
-  })
-
+  var producer = new sfKafka.KProducer(kConfig).createKProducer();
+  await producer.sendKeyMessage("Testmessage", "Here I am rocking like a hurricane");
   setTimeout(() => {
-    kafka.shutdown();
-
-    console.log("Records Count:");
-    console.log(kafka.getKRecordsCount());
-
-    console.log("Messages:");
-    console.log(kafka.getMessages());
-
-    console.log("filterByValue");
-    console.log(kafka.filterByValue('Here I am rocking like a hurricane'));
-
-    console.log("filterByKey");
-    console.log(kafka.filterByKey('Hallo'));
-
-    console.log("All messages")
-    console.log(kafka.getMessages());
-
-    console.log("Letzte Nachricht")
-    console.log(kafka.getLastKRecord());
-
-  }, kConfig.maxConsumingTime*1000);
+    producer.disconnect();
+  }, 5000);
+  //
+  // //const consumer = createConsumer(kConfig);
+  // //consi.createKConsumer(kConfig);
+  // const kafka = new sfKafka.KConsumer(kConfig).createKConsumer();
+  // const sfConsumer = kafka.getConsumer();
+  //
+  // await sfConsumer.run({
+  //   eachMessage: async ({ topic, partition, message }) => {
+  //     //const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+  //     //console.log(`- ${prefix} ${message.key}#${message.value}`)
+  //     kafka.addKRecord(message.key.toString(), message.value.toString(), message.offset, partition)
+  //   },
+  // })
+  //
+  // setTimeout(() => {
+  //   kafka.shutdown();
+  //
+  //   console.log("Records Count:");
+  //   console.log(kafka.getKRecordsCount());
+  //
+  //   console.log("Messages:");
+  //   console.log(kafka.getMessages());
+  //
+  //   console.log("filterByValue");
+  //   console.log(kafka.filterByValue('Here I am rocking like a hurricane'));
+  //
+  //   console.log("filterByKey");
+  //   console.log(kafka.filterByKey('Hallo'));
+  //
+  //   console.log("All messages")
+  //   console.log(kafka.getMessages());
+  //
+  //   console.log("Letzte Nachricht")
+  //   console.log(kafka.getLastKRecord());
+  //
+  // }, kConfig.maxConsumingTime*1000);
 }
 testConsumer();
 
