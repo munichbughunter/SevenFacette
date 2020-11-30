7e1344c42e4b7bcf7e636e01f5d63c0e3ab140e5
fpilz82 <florian.pilz@posteo.de>
2020-11-30 23:20:22 +0100

    Add js implementation

--- core/src/jsMain/kotlin/de/p7s1/qa/sevenfacette/http/HttpResponse.kt
+++ core/src/jsMain/kotlin/de/p7s1/qa/sevenfacette/http/HttpResponse.kt
@@ -1,14 +1,28 @@
 package de.p7s1.qa.sevenfacette.http
 
+import io.ktor.client.statement.*
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 actual class HttpResponse actual constructor(response: HttpResponse) {
     actual val body: String
-        get() = TODO("not implemented yet")
-
     actual val status: Int
-        get() = TODO("not implemented yet")
-
     actual val headers: Map<String, List<String>>
-        get() = TODO("not implemented yet")
+
+    /**
+     * On initialization the Ktor response is used to fill the classes properties.
+     * The body is returned in an asynchronous way so it is inside a runBlocking block
+     */
+    init {
+        var bodyTemp: String = ""
+        GlobalScope.launch {
+            bodyTemp = response.readText()
+        }.onJoin
+
+        this.body = bodyTemp
+        this.status = response.status.value
+        this.headers = response.headers.toMap()
+    }
 }
