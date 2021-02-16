e2a36eb5cea08c311b66c81ffc6ce37fda549c61
munichbughunter <munichbughunter@gmail.com>
2021-02-16 09:47:21 +0100

    :art: add deprecation warning to old dbstatements functions

--- core/src/jvmMain/kotlin/de/p7s1/qa/sevenfacette/db/DbStatements.kt
+++ core/src/jvmMain/kotlin/de/p7s1/qa/sevenfacette/db/DbStatements.kt
@@ -4,51 +4,53 @@ import java.util.Collections
 
 class DbStatements {
     private val statements: MutableList<DbStatement> = mutableListOf()
+    private val statementsAsString: MutableList<String> = mutableListOf()
 
     fun add(dbStatement: DbStatement) {
         statements.add(dbStatement)
     }
 
+    @Deprecated(message = "This function will be deleted in version 2.0.0. Use DbStatement instead.")
+    fun add(statement: String) {
+        statementsAsString.add(statement)
+    }
+
     val list: List<DbStatement>
         get() = Collections.unmodifiableList(statements)
 
+    @Deprecated(message = "This function will be deleted in version 2.0.0. Use get which returns List<DbStatement> instead.")
+    val listStringStatements: List<String>
+        get() = Collections.unmodifiableList(statementsAsString)
+
     operator fun contains(dbStatement: DbStatement) : Boolean {
         return dbStatement in statements
+    }
+
+    @Deprecated(message = "This function will be deleted in version 2.0.0. Use contains(dbStatement: DbStatement) instead.")
+    operator fun contains(statement: String): Boolean {
+        return statement in statementsAsString
     }
 
     operator fun get(index: Int) : DbStatement {
         return statements[index]
     }
 
+    @Deprecated(message = "This function will be deleted in version 2.0.0. Use get() instead.")
+    fun getStatementByIndex(index: Int): String {
+        return statementsAsString[index]
+    }
+
     fun size() : Int {
         return statements.size
     }
 
+    @Deprecated(message = "This function will be deleted in version 2.0.0. Use size() instead.")
+    fun getSize(): Int {
+        return statementsAsString.size
+    }
 
-
-//    val statements: MutableList<String> = mutableListOf()
-//
-//    fun add(statement: String): Boolean {
-//        return statements.add(statement)
-//    }
-//
-//    val list: List<String>
-//        get() = Collections.unmodifiableList(statements)
-//
-//
-//    operator fun contains(statement: String): Boolean {
-//        return statement in statements
-//    }
-//
-//    operator fun get(index: Int): String {
-//        return statements[index]
-//    }
-//
-//    fun size(): Int {
-//        return statements.size
-//    }
-//
-//    fun reformat(index: Int, vararg args: String) {
-//        statements[index] = String.format(statements[index], *args)
-//    }
+    @Deprecated(message = "This function will be deleted in version 2.0.0.")
+    fun reformat(index: Int, vararg args: String) {
+        statementsAsString[index] = String.format(statementsAsString[index], *args)
+    }
 }
