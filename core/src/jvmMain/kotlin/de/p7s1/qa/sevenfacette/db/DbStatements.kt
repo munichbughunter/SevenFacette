b2a994728b0fa940c6fec909b5f61353b824d4ec
munichbughunter <munichbughunter@gmail.com>
2021-02-16 09:35:22 +0100

    :art: change db statements type from string to dbstatement

--- core/src/jvmMain/kotlin/de/p7s1/qa/sevenfacette/db/DbStatements.kt
+++ core/src/jvmMain/kotlin/de/p7s1/qa/sevenfacette/db/DbStatements.kt
@@ -3,52 +3,52 @@ package de.p7s1.qa.sevenfacette.db
 import java.util.Collections
 
 class DbStatements {
-//    private val statements: MutableList<DbStatement> = mutableListOf()
-//
-//    fun add(dbStatement: DbStatement) {
-//        statements.add(dbStatement)
-//    }
-//
-//    val list: List<DbStatement>
-//        get() = Collections.unmodifiableList(statements)
-//
-//    operator fun contains(dbStatement: DbStatement) : Boolean {
-//        return dbStatement in statements
-//    }
-//
-//    operator fun get(index: Int) : DbStatement {
-//        return statements[index]
-//    }
-//
-//    fun size() : Int {
-//        return statements.size
-//    }
+    private val statements: MutableList<DbStatement> = mutableListOf()
 
-
-
-    val statements: MutableList<String> = mutableListOf()
-
-    fun add(statement: String): Boolean {
-        return statements.add(statement)
+    fun add(dbStatement: DbStatement) {
+        statements.add(dbStatement)
     }
 
-    val list: List<String>
+    val list: List<DbStatement>
         get() = Collections.unmodifiableList(statements)
 
-
-    operator fun contains(statement: String): Boolean {
-        return statement in statements
+    operator fun contains(dbStatement: DbStatement) : Boolean {
+        return dbStatement in statements
     }
 
-    operator fun get(index: Int): String {
+    operator fun get(index: Int) : DbStatement {
         return statements[index]
     }
 
-    fun size(): Int {
+    fun size() : Int {
         return statements.size
     }
 
-    fun reformat(index: Int, vararg args: String) {
-        statements[index] = String.format(statements[index], *args)
-    }
+
+
+//    val statements: MutableList<String> = mutableListOf()
+//
+//    fun add(statement: String): Boolean {
+//        return statements.add(statement)
+//    }
+//
+//    val list: List<String>
+//        get() = Collections.unmodifiableList(statements)
+//
+//
+//    operator fun contains(statement: String): Boolean {
+//        return statement in statements
+//    }
+//
+//    operator fun get(index: Int): String {
+//        return statements[index]
+//    }
+//
+//    fun size(): Int {
+//        return statements.size
+//    }
+//
+//    fun reformat(index: Int, vararg args: String) {
+//        statements[index] = String.format(statements[index], *args)
+//    }
 }
