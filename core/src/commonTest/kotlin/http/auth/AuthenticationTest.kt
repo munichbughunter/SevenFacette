7ee63044e951faf0a95b35f6eff2966f1eefe979
munichbughunter <munichbughunter@gmail.com>
2021-03-22 11:32:43 +0100

    Add tests

---
+++ core/src/commonTest/kotlin/http/auth/AuthenticationTest.kt
@@ -0,0 +1,65 @@
+package http.auth
+
+import de.p7s1.qa.sevenfacette.config.ConfigReader
+import de.p7s1.qa.sevenfacette.http.Url
+import de.p7s1.qa.sevenfacette.http.auth.AuthenticationFactory
+import kotlin.test.Test
+import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+/**
+ * TODO: Add Description
+ *
+ * @author Patrick Döring
+ */
+class AuthenticationTest {
+    @Test
+    fun checkBasicAuthentication() {
+        val authMap : MutableMap<String, String> = mutableMapOf()
+        authMap["type"] = "basic"
+        authMap["username"] = "test"
+        authMap["password"] = "test"
+        authMap["realm"] = "test"
+        val authProvider = AuthenticationFactory(authMap).getAuthentication()
+        assertTrue(authProvider.sendWithoutRequest)
+    }
+
+    @Test
+    fun checkOauthOne() {
+        val authMap : MutableMap<String, String> = mutableMapOf()
+        authMap["type"] = "oauth1"
+        authMap["consumer_key"] = "test"
+        authMap["access_token"] = "test"
+        authMap["secret"] = "test"
+        val authProvider = AuthenticationFactory(authMap).getAuthentication()
+        assertTrue(authProvider.sendWithoutRequest)
+    }
+
+    @Test
+    fun checkOauthTwo() {
+        val authMap : MutableMap<String, String> = mutableMapOf()
+        authMap["type"] = "oauth2"
+        authMap["bearer"] = "test"
+        val authProvider = AuthenticationFactory(authMap).getAuthentication()
+        assertTrue(authProvider.sendWithoutRequest)
+    }
+
+    @Test
+    fun checkNoValueType() {
+        val authMap : MutableMap<String, String> = mutableMapOf()
+        authMap["test"] = "test"
+        assertFailsWith<Exception> {
+            val authProvider = AuthenticationFactory(authMap).getAuthentication()
+        }
+    }
+
+    @Test
+    fun checkUnknownType() {
+        val authMap : MutableMap<String, String> = mutableMapOf()
+        authMap["type"] = "test"
+        assertFailsWith<Exception> {
+            val authProvider = AuthenticationFactory(authMap).getAuthentication()
+        }
+    }
+}
