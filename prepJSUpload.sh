d77394b2ce1a134fa4d803fbe8978dc19a507b58
munichbughunter <munichbughunter@gmail.com>
2021-05-07 07:18:36 +0200

    Add replace statements to prepJSUpload

--- prepJSUpload.sh
+++ prepJSUpload.sh
@@ -16,5 +16,14 @@ sed -i -e 's/ktor_ktor_utils_jsLegacy/ktor_ktor_utils_jslegacy/g' SevenFacette-c
 sed -i -e 's/ktor_ktor_client_json_jsLegacy/ktor_ktor_client_json_jslegacy/g' SevenFacette-core.js
 sed -i -e 's/ktor_ktor_client_serialization_jsLegacy/ktor_ktor_client_serialization_jslegacy/g' SevenFacette-core.js
 sed -i -e 's/ktor_ktor_io_jsLegacy/ktor_ktor_io_jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-http-jsLegacy/ktor-ktor-http-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-client-core-jsLegacy/ktor-ktor-client-core-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/kotlinx-serialization-kotlinx-serialization-runtime-jsLegacy/kotlinx-serialization-kotlinx-serialization-runtime-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-client-core-jsLegacy/ktor-ktor-client-core-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-client-auth-jsLegacy/ktor-ktor-client-auth-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-utils-jsLegacy/ktor-ktor-utils-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-client-json-jsLegacy/ktor-ktor-client-json-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-client-serialization-jsLegacy/ktor-ktor-client-serialization-jslegacy/g' SevenFacette-core.js
+sed -i -e 's/ktor-ktor-io-jsLegacy/ktor-ktor-io-jslegacy/g' SevenFacette-core.js
 # Clean up
 rm SevenFacette-core.js-e
